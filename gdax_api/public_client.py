7f5e3daee7be01832d49333e6d1e9759e02a926c
Adil Sadik <sparky.005@gmail.com>
2017-08-13 16:46:30 -0400

    Change 'api' attr to 'endpoint' in PublicClient class

--- gdax_api/public_client.py
+++ gdax_api/public_client.py
@@ -3,35 +3,35 @@ import requests
 
 class PublicClient(object):
     def __init__(self):
-        self.api = 'https://api.gdax.com'
+        self.endpoint= 'https://api.gdax.com'
         pass
 
     def get_products(self):
-        r = requests.get('{}/products'.format(self.api))
+        r = requests.get('{}/products'.format(self.endpoint))
         return r.json()
 
     def get_product_order_book(self, product, level=1):
-        r = requests.get('{}/products/{}/book'.format(self.api, product), data={'level': level})
+        r = requests.get('{}/products/{}/book'.format(self.endpoint, product), data={'level': level})
         return r.json()
 
     def get_product_ticker(self, product):
-        r = requests.get("{}/products/{}/ticker".format(self.api, product))
+        r = requests.get("{}/products/{}/ticker".format(self.endpoint, product))
         return r.json()
 
     def get_trades(self, product):
-        r = requests.get("{}/products/{}/trades".format(self.api, product))
+        r = requests.get("{}/products/{}/trades".format(self.endpoint, product))
         return r.json()
 
     def get_historic_rates(self, product, start=None, end=None, granularity=None):
-        r = requests.get("{}/products/{}/candles".format(self.api, product),
+        r = requests.get("{}/products/{}/candles".format(self.endpoint, product),
                         data={'start': start, 'end': end,
                             'granularity': granularity})
         return r.json()
 
     def get_stats(self, product):
-        r = requests.get("{}/products/{}/stats".format(self.api, product))
+        r = requests.get("{}/products/{}/stats".format(self.endpoint, product))
         return r.json()
 
     def get_currencies(self):
-        r = requests.get("{}/currencies".format(self.api))
+        r = requests.get("{}/currencies".format(self.endpoint))
         return r.json()
